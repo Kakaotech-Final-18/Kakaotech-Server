b24c10343a4a3515f260bf9335ff6392a3aab218
sofia <me@Sofia-MacBook-Pro.local>
2024-12-09 18:53:19 +0900

    bug: cors allow origin list add

--- src/main/java/com/be/parrotalk/config/SecurityConfig.java
+++ src/main/java/com/be/parrotalk/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://dev.ptks.link","https://auth-dev.ptks.link","http://dev.ptks.link"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true); // 쿠키 허용
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
